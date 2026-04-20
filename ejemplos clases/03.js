1384898249281a0c273b81fe528eb5980fb35e56
Alejandro Scarafia <alesscarafia@gmail.com>
2026-04-20 17:55:43 -0300

    ej04

---
+++ ejemplos clases/03.js
@@ -0,0 +1,16 @@
+const nombre = 'pedro'
+
+if (nombre == 'pedro'){
+
+console.log ('hola pedro')
+}
+
+if (nombre == 'pedro'){
+
+console.log ('hola pedro')
+}else {
+
+    console.log ('quien sos')
+}
+
+
