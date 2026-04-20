67f6c4d336144c5da42c7e1eac9c06908904dbb1
Alejandro Scarafia <alesscarafia@gmail.com>
2026-04-20 17:09:45 -0300

    array

--- ejemplos clases/01.js
+++ ejemplos clases/01.js
@@ -9,4 +9,28 @@ const nombre = 'Ale';//tipo string
 const edad = 50; //tipo nro
 const casado = true; //tipo boleano
 
-console.log(`Hola soy ${nombre}`);
+console.log(`Hola soy ${nombre} tengo ${edad} de edad ` );
+
+let contador = 0;
+
+contador = contador + 1;
+
+console.log (contador)
+
+//array
+
+let frutas = ['bananas', 'manzanas', 'peras']
+console.log (frutas)
+
+console.log (frutas[1])
+
+//objeto
+
+let auto ={
+
+    marca : 'ffiat',
+    modelo : '600'
+
+}
+
+console.log (auto.modelo)
